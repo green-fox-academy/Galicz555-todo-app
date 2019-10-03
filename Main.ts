ffd6cd6a499a001c679fd7013a8bb9f57165696d
Galicz Mihály <galicz555@gmail.com>
2019-10-03 11:07:30 +0200

    Corrected the logical base

--- Main.ts
+++ Main.ts
@@ -7,16 +7,41 @@ const fs = require('fs');
 const args: string[] = process.argv; //for using command line arguments. Don't forget that it is a string array w/ atleast 2 elements already!!!
 
 let mainProcess = () => {
-
     if (args.length == 2) {
         fs.readFileSync('list.txt', 'utf-8');
-    // } else if (args[2] == 'list') {
-    //     // 
-    //     console.log('i am listing all the todos');
-    //     // console.log('i am listing all the todos')
-    // } else {
-    //     console.log('this is the error handling');
+    } else if (args[2] == '-l'){
+        
+    } else if (args[2] == '-a'){
+
+    } else if (args[2] == '-r'){
+
+    } else if (args[2] == '-c'){
+
     }
 }
 mainProcess();
 console.log(args)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// } else if (args[2] == 'list') {
+    //     console.log('i am listing all the todos');
+    //     // console.log('i am listing all the todos')
+    // } else {
+    //     console.log('this is the error handling');

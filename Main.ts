dc9fb3c68a7b2b4316a7561242a0a64290ca2024
Galicz Mihály <galicz555@gmail.com>
2019-10-03 11:06:10 +0200

    Created the logical base for the application

--- Main.ts
+++ Main.ts
@@ -9,13 +9,14 @@ const args: string[] = process.argv; //for using command line arguments. Don't f
 let mainProcess = () => {
 
     if (args.length == 2) {
-        console.log('this is without arguments, help');
-    } else if (args[2] == 'list') {
         fs.readFileSync('list.txt', 'utf-8');
-        console.log('i am listing all the todos');
-        // console.log('i am listing all the todos')
-    } else {
-        console.log('this is the error handling');
+    // } else if (args[2] == 'list') {
+    //     // 
+    //     console.log('i am listing all the todos');
+    //     // console.log('i am listing all the todos')
+    // } else {
+    //     console.log('this is the error handling');
     }
 }
 mainProcess();
+console.log(args)

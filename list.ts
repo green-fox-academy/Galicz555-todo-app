2ba7bab47612ece565ab7409fad4245e9b5dbc51
Galicz Mihály <galicz555@gmail.com>
2019-10-03 17:23:13 +0200

    Streamlining

--- list.ts
+++ list.ts
@@ -4,26 +4,23 @@ const fs = require('fs');
 export class list {
     public _list: string[]
 
-    constructor(list: string[] = []) {
-        this._list = list;
+    constructor() {
+        this._list = fs.readFileSync('list.txt', 'utf-8').split("\r\n");
     }
 
     public isItEmpty():boolean{
-        this._list = fs.readFileSync('list.txt', 'utf-8').split("\r\n");
         if(!this._list[0]){
         return true;
         };
     }
 
     public addTask(task: string) {
-        this._list = fs.readFileSync('list.txt', 'utf-8').split("\r\n");
         this._list.push(task);
         let data2 = this._list.join("\r\n");
         fs.writeFileSync('list.txt', data2);
     }
 
     public listCounter() {
-        this._list = fs.readFileSync('list.txt', 'utf-8').split("\r\n");
         for (let i = 0; i < this._list.length; i++) {
             console.log((i + 1) + " - " + this._list[i]);
         }
